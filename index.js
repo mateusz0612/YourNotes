20ccabcf05af4c826ce7064c0bc28ea950fbbf18
mateusz0612 <dennyfacio@gmail.com>
2020-09-15 00:12:35 +0200

    Added scripts/more styling/Have to add removing single note function

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
 const noteTitle = document.querySelector('#note-title');
 const noteContent = document.querySelector('#note-content');
+const noteContainer = document.querySelector('.container_sec');
 const addIcon = document.querySelector('.plus');
+const removeBtn = document.querySelector('.remove');
+
+const checkIfEmpty = () => {
+    if (noteTitle.value == '' || noteContent.value == '') {
+        alert('Title and content has to be given');
+        return false;
+    }
+    return true;
+};
+
+const clearInputs = () => {
+    noteTitle.value = '';
+    noteContent.value = '';
+}
+
+const createNote = () => {
+    const mainNote = document.createElement('div');
+    mainNote.classList.add('note');
+    const title = document.createElement('h2');
+    const content = document.createElement('p');
+    title.classList.add('noteH2');
+    content.classList.add('noteP');
+    title.textContent = noteTitle.value.toUpperCase();
+    content.textContent = noteContent.value;
+    mainNote.appendChild(title);
+    mainNote.appendChild(content);
+    noteContainer.appendChild(mainNote);
+};
+
+const removeAllNotes = () => {
+    const notes = document.querySelectorAll('.note');
+    notes.forEach(element => element.remove());
+};
+
+addIcon.addEventListener('click', () => {
+    if (checkIfEmpty()) createNote();
+    clearInputs();
+});
+
+removeBtn.addEventListener('click', () => {
+    let result = confirm('Are you suer you want to delete ALL your notes?')
+    if (result) removeAllNotes();
+    else return;
+});
